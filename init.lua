25bcc7703c3121a2865d923a9e96be1b59336ecc
Oleh Vorobiov <oleh.vorobiov@hobovrlabs.org>
2024-05-09 11:36:07 +0300

    init.lua: Fixed clangd header autoinclude
    Signed-off-by: Oleh Vorobiov <oleh.vorobiov@hobovrlabs.org>

--- init.lua
+++ init.lua
@@ -534,7 +534,7 @@ require("lazy").setup({
 						"clangd",
 						"--background-index",
 						"--clang-tidy",
-						"--header-insertion=iwyu",
+						"--header-insertion=never",
 						"--completion-style=detailed",
 						"--function-arg-placeholders",
 						"--fallback-style=webkit",
